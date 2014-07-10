812926b45c3e44258cd09a619b210e9f4df560c7
Hong <hongj77@gmail.com>
2014-07-09 19:39:22 -0500

    implemented option select

--- mergejuicer.py
+++ mergejuicer.py
@@ -1,21 +1,43 @@
 import os
  
 replacements = {'o': '0', 'l': '1', 'b': '8'}
- 
-for filename in os.listdir(os.getcwd()):
- 
-	if filename != 'mergejuicer.py':
- 
-		infile = open(filename, 'r')
-		outfile = open('temp', 'w+')
- 
-		for line in infile:
-			for key, value in replacements.iteritems():
-				line = line.replace(key, value)
-			outfile.write(line)
-		infile.close()
-		outfile.close()
- 
-		os.remove(filename)
-		os.rename('temp', filename)
-		print '*', filename, ' is juiced'
+exclude = ['mergejuicer.py', 'README.md', '.git']
+juiceTypes = ['brutal', 'distracting', 'annoying']
+
+def brutal (): 
+	for filename in os.listdir(os.getcwd()):
+	 
+		if filename not in exclude:
+	 
+			infile = open(filename, 'r')
+			outfile = open('temp', 'w+')
+	 
+			for line in infile:
+				for key, value in replacements.iteritems():
+					line = line.replace(key, value)
+				outfile.write(line)
+			infile.close()
+			outfile.close()
+	 
+			os.remove(filename)
+			os.rename('temp', filename)
+			print '*', filename, ' is juiced'
+
+
+
+print "How would you like to juice?"
+
+for i, val in enumerate(juiceTypes):
+	print "press ", i+1, "for ",val.upper()
+
+answer = raw_input('>>>')
+
+if int(answer) == 1:
+	brutal()
+# elif int(answer) == 2:
+# 	distract()
+# elif int(answer) == 3:
+# 	annoying()
+
+
+
