561d7599a14aad01b704133429ab8e915d0b96ed
Hong Jeon <hongj77@gmail.com>
2014-07-10 11:03:15 -0500

    added distract

--- mergejuicer.py
+++ mergejuicer.py
@@ -1,14 +1,13 @@
 import os
- 
-replacements = {'o': '0', 'l': '1', 'b': '8'}
+
 exclude = ['mergejuicer.py', 'README.md', '.git']
 juiceTypes = ['brutal', 'distracting', 'annoying']
 
 def brutal (): 
+	replacements = {'o': '0', 'l': '1', 'b': '8'}
+
 	for filename in os.listdir(os.getcwd()):
-	 
 		if filename not in exclude:
-	 
 			infile = open(filename, 'r')
 			outfile = open('temp', 'w+')
 	 
@@ -24,6 +23,29 @@ def brutal ():
 			print '*', filename, ' is juiced'
 
 
+def distract():
+	replacements = {'9':'6', '--': '-'}
+
+	for filename in os.listdir(os.getcwd()):
+		if filename not in exclude:
+			infile = open(filename, 'r')
+			outfile = open('temp', 'w+')
+
+			count = 0;
+			for line in infile:		
+				for key, value in replacements.iteritems():
+					if count % 2 == 0:
+						line = line.replace(key, value)
+				count += 1
+				outfile.write(line)
+
+			infile.close()
+			outfile.close()
+
+			os.remove(filename)
+			os.rename('temp', filename)
+			print '*', filename, ' is juiced'
+
 
 print "How would you like to juice?"
 
@@ -34,8 +56,8 @@ answer = raw_input('>>>')
 
 if int(answer) == 1:
 	brutal()
-# elif int(answer) == 2:
-# 	distract()
+elif int(answer) == 2:
+	distract()
 # elif int(answer) == 3:
 # 	annoying()
 
